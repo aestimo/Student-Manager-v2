6a69248b3e97b64ab83fb3bafd7c2f99ff84404d
aestimo <lee.njuguna@gmail.com>
2013-07-23 14:51:44 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentManagerV2::Application.config.secret_token = '2fbf9ab840c754eb7383c9e33394d164fa444033fc89b70b9b16953f678e2abbd556ba7125e1c235c2d0e30ab247d811f356a603c1fcd29a1e4e06789d5a003e'
